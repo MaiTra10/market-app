628499cb5b0625d8a859e0b9f7e8e6b9f29105c7
Maitra Patel <10maitra22@gmail.com>
2023-08-24 20:20:31 -0600

    feat
    added a details page for each item and related items are shown on that page

--- market_app/item/views.py
+++ market_app/item/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Item
 
 # Create your views here.
+
+def details(request, pk):
+
+    item = get_object_or_404(Item, pk = pk)
+    # Gets first 3 items in the same category as the 'item' that are not sold, and excludes the item that has the same primary key
+    related_items = Item.objects.filter(category = item.category, is_sold = False).exclude(pk = pk)[:3]
+
+    return render(request, "item/detail.html", {
+
+        "item": item,
+        "related_items": related_items
+
+    })
