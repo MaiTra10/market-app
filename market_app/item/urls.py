9e4780e49241abfe026ef336d1ef6b09e3843db1
Maitra Patel <10maitra22@gmail.com>
2023-08-26 00:30:33 -0600

    feat
    added a way to create new items and made a dashboard to see all your items. Also you can edit the items after creation and delete them.

--- market_app/item/urls.py
+++ market_app/item/urls.py
@@ -8,6 +8,9 @@ app_name = "item"
 
 urlpatterns = [
     
+    path("add/", views.add, name = "add"),
+    path("<int:pk>/delete/", views.delete, name = "delete"),
+    path("<int:pk>/edit/", views.edit, name = "edit"),
     path("<int:pk>/", views.details, name = "detail")
 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- market_app/item/views.py
+++ market_app/item/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Item
+from .forms import AddItemForm, EditItemForm
 
 # Create your views here.
 
@@ -13,5 +15,67 @@ def details(request, pk):
 
         "item": item,
         "related_items": related_items
+
+    })
+
+@login_required
+def add(request):
+
+    if request.method == "POST":
+
+        form = AddItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+
+            item = form.save(commit = False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect("item:detail", pk = item.id)
+
+    else:
+    
+        form = AddItemForm()
+
+    return render(request, "item/form.html", {
+
+        "form": form,
+        "title": "Add Item",
+        "button": "Create"
+
+    })
+
+@login_required
+def delete(request, pk):
+
+    item = get_object_or_404(Item, pk = pk, created_by = request.user)
+    item.delete()
+
+    return redirect("dashboard:index")
+
+@login_required
+def edit(request, pk):
+
+    item = get_object_or_404(Item, pk = pk, created_by = request.user)
+
+    if request.method == "POST":
+
+        form = EditItemForm(request.POST, request.FILES, instance = item)
+
+        if form.is_valid():
+
+            item.save()
+
+            return redirect("item:detail", pk = item.id)
+
+    else:
+
+        form = EditItemForm(instance = item)
+
+    return render(request, "item/form.html", {
+
+        "form": form,
+        "title": "Edit Item",
+        "button": "Update"
 
     })
